e7186a98aea88f95312287c56b19bb721feb8052
David Kis <cf.paintech@gmail.com>
2021-08-18 18:27:44 +0200

    image-spec: turn off dnsmasq's dns function to use the potential home router's

--- image-specs/stage0/02-enable-access-point/00-run.sh
+++ image-specs/stage0/02-enable-access-point/00-run.sh
@@ -12,9 +12,6 @@ install -m 644 files/br0-member-wlan0.network "${ROOTFS_DIR}/etc/systemd/network
 install -m 755 files/p4pi-setup "${ROOTFS_DIR}/usr/sbin/"
 install -m 644 files/p4pi-setup.service "${ROOTFS_DIR}/lib/systemd/system/"
 
-# Switch off binding to port 53 to avoid conflict with dnsmasq
-install -m 644 files/resolved.conf "${ROOTFS_DIR}/etc/systemd/"
-
 on_chroot << EOF
 
 # Enable IPv4 routing
@@ -23,9 +20,6 @@ echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.d/routed-ap.conf
 # Enable access point
 systemctl unmask hostapd
 systemctl enable hostapd
-
-# Setup DNS if available
-systemctl enable systemd-resolved.service
 
 # Create and setup interfaces on startup
 systemctl enable systemd-networkd.service
