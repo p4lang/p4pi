5ecddba0a0761429ef8c6a3795a127a0d593da45
Dávid Kis <kidraai@inf.elte.hu>
2023-02-06 15:03:59 +0100

    fix(pi-gen): correct custom kernel path

--- pi-gen/stage3/01-p4pi/02-run.sh
+++ pi-gen/stage3/01-p4pi/02-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-mv vmlinuz-5.15.84-v8+ p4pi-kernel8.img
-echo "kernel=p4pi-kernel8.img" >> config.txt
+mv /boot/vmlinuz-5.15.84-v8-p4pi /boot/p4pi-kernel8.img
+echo "kernel=p4pi-kernel8.img" >> /boot/config.txt
 EOF
